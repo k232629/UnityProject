b7b944caf24a10792877a94ff93073c1ac7a6e59
k232629 <olhakaraiman@gmail.com>
2017-05-26 12:17:11 +0300

    Scripts

--- Assets/Scripts/HeroRabit.cs
+++ Assets/Scripts/HeroRabit.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class HeroRabit : MonoBehaviour {
 
+	bool isGrounded = false;
+	bool JumpActive = false;
+	float JumpTime = 0f;
+	public float MaxJumpTime = 2f;
+	public float JumpSpeed = 2f;
+	
 	public float speed = 1;
-	float value = Input.GetAxis ("Horizontal");
+	
+	
 	Rigidbody2D myBody = null;
 	SpriteRenderer sr = null;
 	// Use this for initialization
 	void Start () {
 		myBody = this.GetComponent<Rigidbody2D> ();
 		sr = GetComponent<SpriteRenderer>();
+		LevelController.current.setStartPosition (transform.position);
+
 	}
 	
 	// Update is called once per frame
@@ -21,7 +30,39 @@ public class HeroRabit : MonoBehaviour {
 
 	void FixedUpdate () {
 		//[-1, 1]
+
+		Vector3 from = transform.position + Vector3.up * 0.3f;
+		Vector3 to = transform.position + Vector3.down * 0.1f;
+		int layer_id = 1 << LayerMask.NameToLayer ("Ground");
+		//Перевіряємо чи проходить лінія через Collider з шаром Ground
+		RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+		if(hit) {
+			isGrounded = true;
+		} else {
+			isGrounded = false;
+		}
+		//Намалювати лінію (для розробника)
+
+		if(Input.GetButtonDown("Jump") && isGrounded) {
+			this.JumpActive = true;
+		}
+		if(this.JumpActive) {
+			//Якщо кнопку ще тримають
+			if(Input.GetButton("Jump")) {
+				this.JumpTime += Time.deltaTime;
+				if (this.JumpTime < this.MaxJumpTime) {
+					Vector2 vel = myBody.velocity;
+					vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
+					myBody.velocity = vel;
+				}
+			} else {
+		this.JumpActive = false;
+		this.JumpTime = 0;
+		}
+}
 		float value = Input.GetAxis ("Horizontal");
+		Animator animator = GetComponent<Animator> ();
+
 		if (Mathf.Abs (value) > 0) {
 			Vector2 vel = myBody.velocity;
 			vel.x = value * speed;
@@ -33,5 +74,20 @@ public class HeroRabit : MonoBehaviour {
 		} else if(value > 0) {
 			sr.flipX = false;
 		}
+
+		if(Mathf.Abs(value) > 0) {
+			animator.SetBool ("run", true);
+		} else {
+			animator.SetBool ("run", false);
+		}
+		if(this.isGrounded) {
+			animator.SetBool ("jump", false);
+		} else {
+			animator.SetBool ("jump", true);
+		}
+	
 	}
+
+
+	
 }
